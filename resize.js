cd6325e9d0b6765a9045f1f49ebbfcc98f01a42b
tim-mawson <tim.mawson@uk.ibm.com>
2017-11-16 23:40:59 +0000

    Fix image loading

--- resize.js
+++ resize.js
@@ -1,28 +1,43 @@
-const { createCanvas, Image } = require("canvas");
-const request = require("request");
+const { createCanvas, Image } = require('canvas');
+const request = require('request');
 
 function resize(imgUrl, height, cb) {
   request(imgUrl, { encoding: null }, (err, res) => {
+    if (err) {
+      throw new Error(err);
+    }
+
     const source = new Image();
+
+    source.onerror = err => {
+      throw new Error(err);
+    };
+
+    source.onload = () => {
+      const srcHeight = source.height;
+      const srcWidth = source.width;
+      height = height || srcHeight;
+
+      const targetWidth = srcWidth / srcHeight * height;
+
+      const canvas = createCanvas(targetWidth, parseInt(height, 10));
+      const ctx = canvas.getContext('2d');
+      ctx.patternQuality = 'bilinear';
+      ctx.filter = 'bilinear';
+      ctx.antialias = 'subpixel';
+      ctx.imageSmoothingEnabled = true;
+      ctx.drawImage(source, 0, 0, targetWidth, height);
+
+      const out = canvas.toBuffer((err, buf) => {
+        if (err) {
+          throw err;
+        }
+
+        cb(buf);
+      });
+    };
+
     source.src = res.body;
-
-    const srcHeight = source.height;
-    const srcWidth = source.width;
-    height = height || srcHeight;
-
-    const targetWidth = srcWidth / srcHeight * height;
-
-    const canvas = createCanvas(targetWidth, parseInt(height, 10));
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(source, 0, 0, targetWidth, height);
-
-    const out = canvas.toBuffer((err, buf) => {
-      if (err) {
-        throw err;
-      }
-
-      cb(buf);
-    });
   });
 }
 
