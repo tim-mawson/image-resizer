6262813c11f4525d68cf303148ad1c4838b7debc
tim-mawson <tim.mawson@uk.ibm.com>
2017-11-16 19:11:50 +0000

    Move resize endpoint to root

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const LISTEN_PORT = process.env.IMGRESIZE_PORT || 3333;
 
-const resize = require('./resize');
+const resize = require("./resize");
 
-app.get('/resize', (req, res) => {
-    res.setHeader("Content-Type", "image/png");
-    resize(req.query.url, req.query.height, img => res.send(img));
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "image/png");
+  resize(req.query.url, req.query.height, img => res.send(img));
 });
 
-app.listen(LISTEN_PORT, () => console.log('Application listening on port', LISTEN_PORT));
+app.listen(LISTEN_PORT, () =>
+  console.log("Application listening on port", LISTEN_PORT)
+);
--- resize.js
+++ resize.js
@@ -1,26 +1,26 @@
-const { createCanvas, Image } = require('canvas');
-const request = require('request');
+const { createCanvas, Image } = require("canvas");
+const request = require("request");
 
 function resize(imgUrl, height, cb) {
-    request(imgUrl, { encoding: null }, (err, res) => {
-        const source = new Image();
-        source.src = res.body;
+  request(imgUrl, { encoding: null }, (err, res) => {
+    const source = new Image();
+    source.src = res.body;
 
-        const srcHeight = source.height;
-        const srcWidth = source.width;
-        const targetWidth = (srcWidth / srcHeight) * height;
-        const canvas = createCanvas(targetWidth, parseInt(height, 10));
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(source, 0, 0, targetWidth, height);
-        
-        const out = canvas.toBuffer((err, buf) => {
-            if (err) {
-                throw err
-            }
+    const srcHeight = source.height;
+    const srcWidth = source.width;
+    const targetWidth = srcWidth / srcHeight * height;
+    const canvas = createCanvas(targetWidth, parseInt(height, 10));
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(source, 0, 0, targetWidth, height);
 
-            cb(buf);
-        })
+    const out = canvas.toBuffer((err, buf) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(buf);
     });
+  });
 }
 
 module.exports = resize;
