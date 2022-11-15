41615ac8f62b69b1221cf5609ad38004d17279e6
Tim@HOMER.cable.virginm.net <unknown>
2022-11-15 22:12:23 +0000

    Switch to ES6 and node-fetch

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const LISTEN_PORT = process.env.IMGRESIZE_PORT || 3333;
 
-app.use(require('express-status-monitor')());
+import resize from './resize.js';
 
-const resize = require('./resize');
-
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
   res.setHeader('Content-Type', 'image/png');
   const imageUrl = Buffer.from(req.path.slice(1), 'base64').toString('utf8');
 
-  resize(
+  const image = await resize(
     imageUrl,
     req.query.width,
     req.query.height,
     req.query.keepRatio,
-    img => res.send(img)
   );
+
+  res.send(image)
 });
 
 app.listen(LISTEN_PORT, () =>
